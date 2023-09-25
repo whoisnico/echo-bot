c6154b3e2e68afcc0e139a893ac65b4d1cecd104
whoisnico <104150729+whoisnico@users.noreply.github.com>
2023-09-25 20:56:43 +0200

    dev version 0.0.1
    (+) new comment system
    -> dev version, beta version, release version
    (+) Sellpass customer role
    (-) Sellpass API is not working

--- bot/cogs/commands/sellpass.py
+++ bot/cogs/commands/sellpass.py
@@ -115,6 +115,38 @@ class SellpassCog(commands.Cog):
             await inter.send(embed=embed, ephemeral=True)
 
 
+    @commands.slash_command()
+    async def shop_claimrole(self, inter, orderid):
+            """Get your customer role!"""
+            try:
+                with open("database/sellpass/shop.json", "r") as json_file:
+                    data = json.load(json_file)
+                    if data:
+                        if not "Shop" in data and "ID" in data["Shop"] and "Token" in data["Shop"] and "ShopID" in data["Shop"] and "CustomerRole" in data["Shop"]:
+                            embed = disnake.Embed(title="Sellpass Connection Setup", color=0x2F3136, description=f"**Error:** You have to set first your token & shopid! \n [Click for Help](https://whoisnico.github.io/echo-bot/) | `/setshop_token` | `/setshop_id`")
+                            embed.set_thumbnail(url="https://www.sythe.org/data/avatars/l/1560/1560703.jpg")
+                            await inter.send(embed=embed, ephemeral=True)
+                            return
+                    else:
+                        embed = disnake.Embed(title="Sellpass Connection Setup", color=0x2F3136, description=f"**Error:** You have to set first your token & shopid! \n [Click for Help](https://whoisnico.github.io/echo-bot/) | `/setshop_token` | `/setshop_id`")
+                        embed.set_thumbnail(url="https://www.sythe.org/data/avatars/l/1560/1560703.jpg")
+                        await inter.send(embed=embed, ephemeral=True)
+                        return
+            except:
+                embed = disnake.Embed(title="Sellpass Connection Setup", color=0x2F3136, description=f"**Error:** You have to set first your token & shopid! \n [Click for Help](https://whoisnico.github.io/echo-bot/) | `/setshop_token` | `/setshop_id`")
+                embed.set_thumbnail(url="https://www.sythe.org/data/avatars/l/1560/1560703.jpg")
+                await inter.send(embed=embed, ephemeral=True)
+                return
+            with open("database/sellpass/shop.json", "r") as json_file:
+                data = json.load(json_file)
+            
+            role= data["Shop"]["CustomerRole"]
+            authtoken= data["Shop"]["Token"]
+            shopId= data["Shop"]["ShopID"]
+            response = requests.get(f'https://dev.sellpass.io/self/{shopId}/invoices/{orderid}', headers={'Authorization': authtoken, 'Content-Type': 'application/json'})
+            response_json = json.loads(response.text)
+            print(response_json)
+            
     
 
 def setup(bot):
